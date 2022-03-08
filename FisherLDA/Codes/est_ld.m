8d4d367bdcbcbe738847fef2e63016ba948cac77
MR-dot-15 <rmukherjee1505@gmail.com>
2022-03-08 10:39:48 +0530

    Updated from Win

--- FisherLDA/Codes/est_ld.m
+++
@@ -1,7 +0,0 @@
-function V = est_ld(X)
-% find the eigenvals and vecs
-[V, L] = eig(X);
-% sort the eigenvals (ascend)
-[~, ind] = sort(diag(L), 'descend');
-% sort correspodning eigvecs
-V = V(:, ind);
